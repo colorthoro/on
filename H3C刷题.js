40b8bcf741cc8ddb2bab5c4c9e60ce2bb7c5e053
colorthoro <dream.0112@qq.com>
2023-07-21 18:28:56 +0800

    H3C刷题

---
+++ H3C刷题.js
@@ -0,0 +1,52 @@
+// 呼出做题界面的控制台，直接粘贴
+async function myFrame(src, func, id) {
+    let frame = document.createElement('iframe');
+    document.body.appendChild(frame);
+    frame.src = src, frame.id = id, frame.contentWindow.id = id;
+    let ok, workResPromise = new Promise(i => ok = i);
+    frame.onload = () => ok(func(frame));
+    return { frame, workRes: await workResPromise };
+}
+
+myFrame(location.href, async (frame) => {
+    let doc = frame.contentWindow.document;
+    doc.querySelectorAll('.cs-test-item').forEach(item => {
+        item.querySelectorAll('div.cs-test-option:nth-child(2n)').forEach(option => {
+            option.click();
+        })
+    })
+    await new Promise(ok => setTimeout(ok, 500));
+    doc.querySelectorAll('#goNext')[0].click();
+    await new Promise(ok => setTimeout(ok, 500));
+    doc.querySelectorAll('a.layui-layer-btn1')[0].click();
+    await new Promise(ok => setTimeout(ok, 500));
+    doc.querySelectorAll('a.layui-layer-btn0')[0].click();
+    let ok, loadedPromise = new Promise(i => ok = i);
+    frame.onload = ok;
+    await loadedPromise;
+}, 'myFrame-1').then(async res => {
+    let cw = res.frame.contentWindow, rightDataMap=cw.rightDataMap;
+    let src = cw.location.href.replace('viewCourseExamPage', 'enterCourse');
+    return myFrame(src, async (frame) => {
+        let cw = frame.contentWindow, doc = cw.document;
+        for (let k in rightDataMap) {
+            let v = rightDataMap[k];
+            let inputs = doc.getElementsByName(k.split('_')[1]);
+            console.log(k, v, inputs);
+            await new Promise(ok => setTimeout(ok, 500));
+            inputs.forEach(input => {
+                let abc = $(input).parent().next().text();
+                let info = $(input).parent().parent().next().text();
+                let option = $(input).parent().parent().parent();
+                console.log(abc, info);
+                if (!input.checked && (v.includes(abc) || v.includes(info))) option.click();
+                else if (input.checked) option.click();
+            });
+        }
+        await new Promise(ok => setTimeout(ok, 500));
+        doc.querySelectorAll('#goNext')[0].click();
+        await new Promise(ok => setTimeout(ok, 500));
+        doc.querySelectorAll('a.layui-layer-btn1')[0].click();
+        location.reload();
+    }, 'myFrame-2');
+});
---
+++ H3C刷题（可视）.js
@@ -0,0 +1,55 @@
+// 呼出做题界面的控制台，直接粘贴
+function inFrame(src, func, id = 'inFrame-1', ppp = window) {
+    let frame = `<frameset cols='*'>\n<frame id='${id}' src='${src}' ></frameset>`;
+    ppp.document.write(frame);
+    let fw = $(ppp.document).find('#' + id)[0].contentWindow;
+    ppp.fw = fw;
+    fw.id = id;
+    fw.ppp = ppp;
+    fw.onload = () => { func(fw); };
+}
+
+async function correct(fw) {
+    await new Promise(ok => setTimeout(ok, 1000));
+    fw.document.querySelectorAll('#goNext')[0].click();
+    await new Promise(ok => setTimeout(ok, 1000));
+    fw.document.querySelectorAll('a.layui-layer-btn1')[0].click();
+    await new Promise(ok => setTimeout(ok, 1000));
+    let close = fw.document.querySelectorAll('.layui-layer-close1')[0];
+    if (close) close.click();
+    await new Promise(ok => setTimeout(ok, 1000));
+}
+
+inFrame(location.href, async (fw) => {
+    console.log(fw.id, fw.ppp.id);
+    $(fw.document).find('.cs-test-item').each((i, item) => {
+        $(item).find('div.cs-test-option:nth-child(2n)').each((j, option) => {
+            $(option).click();
+        })
+    });
+    await correct(fw);
+    let src = fw.location.href.replace('enterCourse', 'viewCourseExamPage') + `&examUserId=${info.examUserId}`
+    inFrame(src, (fw) => {
+        console.log(fw.id, fw.ppp.id);
+        let src = location.href.replace('viewCourseExamPage', 'enterCourse');
+        inFrame(src, async (fw) => {
+            console.log(fw.id, fw.ppp.id);
+            rightDataMap = fw.ppp.rightDataMap;
+            console.log(rightDataMap);
+            for (let k in rightDataMap) {
+                let v = rightDataMap[k];
+                let inputs = fw.document.getElementsByName(k.split('_')[1]);
+                // console.log(k, v, inputs);
+                inputs.forEach(input => {
+                    let abc = $(input).parent().next().text();
+                    let info = $(input).parent().parent().next().text();
+                    let option = $(input).parent().parent().parent();
+                    // console.log(abc, info);
+                    if (!input.checked && (v.includes(abc) || v.includes(info))) option.click();
+                    else if (input.checked) option.click();
+                });
+            }
+            await correct(fw);
+        }, 'inFrame-3', fw)
+    }, 'inFrame-2', fw)
+}, 'inFrame-1', window)
