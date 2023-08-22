fbc05e0710bec3110f74990fcecaf418e25ab891
colorthoro <dream.0112@qq.com>
2023-08-22 10:33:54 +0800

    update clash p

--- clash_parser.js
+++ clash_parser.js
@@ -1,5 +1,11 @@
 /**
 parsers:
+  - reg: .*www.ccsub.online.+$
+    remote:
+      url: 'https://colorthoro.gitee.io/on-js/clash_parser.js'
+      cache: false
+
+parsers:
   - reg: .*www.ccsub.online/.+$   # 第一个执行的parser
     file: '{path_to_this_file}'
 */
@@ -25,6 +31,14 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
     obj.rules.unshift('DOMAIN-KEYWORD,openai.com,☝openai');
     obj.rules.unshift('DOMAIN-KEYWORD,google,☝openai');
     notify('预处理', `成功添加 ☝openai 规则`);
+    for(let select of selected){
+        if(select.name == '☝openai'){
+            select.now = '江苏联通转日本NTT5[倍率:0.8]'
+        }else if(select.name.includes('国外')){
+            select.now = '深港专线转香港BGP[M][倍率:2.5]'
+        }
+        console.log(select.name, select.now)
+    }
     console.log('预处理成功')
     return yaml.stringify(obj)
 }
