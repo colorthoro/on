a6ab229c2e63da6075ed4797c149e5fdd30c7664
colorthoro <dream.0112@qq.com>
2023-09-16 00:28:25 +0800

    1

--- clash_parser.js
+++ clash_parser.js
@@ -15,28 +15,51 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
     console.log('js模块开始预处理')
     const obj = yaml.parse(raw)
     console.log(obj, name, url, interval, selected);
-    let proxies = null;
+    let proxies = obj['proxies'];
     let groups = obj['proxy-groups'];
-    for(let group of groups){
-        if(group.name.includes('国外')){
-            proxies = group.proxies;
-            break;
-        }
-    }
+
     groups.push({
-        name:'☝openai',
-        type:'select',
+        name: '☝openai',
+        type: 'select',
         proxies
     });
-    obj.rules = obj.rules.filter((v)=>!v.includes(',google,'))
+
+    let proxiesLocationMap = new Map();
+    proxies.forEach(v => {
+        let match = v.match(/转([^a-zA-Z\[\]]+)/);
+        if (!match) return;
+        let loc = match[1];
+        if (!proxiesLocationMap.get(loc)) proxiesLocationMap.set(loc, []);
+        else proxiesLocationMap.get(loc).push(v);
+    });
+    let groupsLocationMap = new Map();
+    proxiesLocationMap.forEach((v, k) => {
+        let obj = {
+            name: k + '最优',
+            type: 'url-test',
+            proxies: v,
+            url: 'http://www.gstatic.com/generate_204',
+            interval: 300,
+        };
+        groupsLocationMap.set(k, obj);
+        groups.push(obj);
+    })
+
+    groups.forEach(v => {
+        if (v.name.includes('国外')) {
+            let HK = groupsLocationMap.get('香港');
+            if (HK) v.proxies = [HK];
+        }
+    })
+
+    obj.rules = obj.rules.filter((v) => !v.includes(',google,'))
     obj.rules.unshift('DOMAIN-KEYWORD,openai.com,☝openai');
     obj.rules.unshift('DOMAIN-KEYWORD,google,☝openai');
-    notify('预处理', `成功添加 ☝openai 规则`);
-    if(selected) for(let select of selected){
-        if(select.name == '☝openai'){
+
+
+    if (selected) for (let select of selected) {
+        if (select.name == '☝openai') {
             select.now = '广东移动转日本NTT2[倍率:0.8]'
-        }else if(select.name.includes('国外')){
-            select.now = '深港专线转香港BGP[M][倍率:2.5]'
         }
         console.log(select.name, select.now)
     }
