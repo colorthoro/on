798760d8346405629f88d2c88f3326b978107f16
colorthoro <dream.0112@qq.com>
2023-09-15 23:03:31 +0800

    1

--- clash_parser.js
+++ clash_parser.js
@@ -12,6 +12,7 @@ parsers:
 
 
 module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
+    console.log(name, url, interval, selected);
     console.log('js模块开始预处理')
     const obj = yaml.parse(raw)
     let proxies = null;
@@ -31,7 +32,7 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
     obj.rules.unshift('DOMAIN-KEYWORD,openai.com,☝openai');
     obj.rules.unshift('DOMAIN-KEYWORD,google,☝openai');
     notify('预处理', `成功添加 ☝openai 规则`);
-    for(let select of selected){
+    if(selected) for(let select of selected){
         if(select.name == '☝openai'){
             select.now = '广东移动转日本NTT2[倍率:0.8]'
         }else if(select.name.includes('国外')){
@@ -48,27 +49,36 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
 parsers:
   - reg: .*www.ccsub.online.+$   # 第一个执行的parser
     code: |
-      module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
-        console.log('开始预处理')
-        const obj = yaml.parse(raw)
-        let proxies = null;
-        let groups = obj['proxy-groups'];
-        for(let group of groups){
-            if(group.name.includes('国外')){
-                proxies = group.proxies;
-                break;
+        module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
+            console.log(name, url, interval, selected);
+            console.log('js模块开始预处理')
+            const obj = yaml.parse(raw)
+            let proxies = null;
+            let groups = obj['proxy-groups'];
+            for(let group of groups){
+                if(group.name.includes('国外')){
+                    proxies = group.proxies;
+                    break;
+                }
             }
+            groups.push({
+                name:'☝openai',
+                type:'select',
+                proxies
+            });
+            [].filter((v)=>!v.includes(',google,'))
+            obj.rules.unshift('DOMAIN-KEYWORD,openai.com,☝openai');
+            obj.rules.unshift('DOMAIN-KEYWORD,google,☝openai');
+            notify('预处理', `成功添加 ☝openai 规则`);
+            if(selected) for(let select of selected){
+                if(select.name == '☝openai'){
+                    select.now = '广东移动转日本NTT2[倍率:0.8]'
+                }else if(select.name.includes('国外')){
+                    select.now = '深港专线转香港BGP[M][倍率:2.5]'
+                }
+                console.log(select.name, select.now)
+            }
+            console.log('预处理成功')
+            return yaml.stringify(obj)
         }
-        groups.push({
-            name:'☝openai',
-            type:'select',
-            proxies
-        });
-        [].filter((v)=>!v.includes(',google,'))
-        obj.rules.unshift('DOMAIN-KEYWORD,openai.com,☝openai');
-        obj.rules.unshift('DOMAIN-KEYWORD,google,☝openai');
-        notify('预处理', `成功添加 ☝openai 规则`);
-        console.log('预处理成功')
-        return yaml.stringify(obj)
-      }
  */
