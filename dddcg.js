c20633574e56ec0cd0a1d6e0e048b193738912b3
colorthoro <dream.0112@qq.com>
2023-11-05 15:27:01 +0800

    1

--- dddcg.js
+++ dddcg.js
@@ -1,18 +1,18 @@
 // DDNS Downloader Client with Gitee
-(() => {
+(async () => {
     const GITEE_TOKEN = '4afb627b2979b9d5c653246be733ab27'
     const GITEE_GIST_ID = 'k9b54vytfj2g3xa7hoi0n78'
     let content;
-    let ok, f, p = new Promise((i, j) => (ok = i, f = j));
+    let ok, err, p = new Promise((i, j) => (ok = i, err = j));
     try { window && (window.dns = p); } catch (e) { window = null; }
-    fetch(`https://gitee.com/api/v5/gists/${GITEE_GIST_ID}?access_token=${GITEE_TOKEN}`).then(res => res.json()).then(res => {
-        try {
-            content = res['files']['dns.json']['content']
-            window && ok(JSON.parse(content));
-            console.log('dddcg get success', content);
-        } catch (e) {
-            console.error('dddcg get nothing');
-            window && f(e);
-        }
-    })
+    let res = await fetch(`https://gitee.com/api/v5/gists/${GITEE_GIST_ID}?access_token=${GITEE_TOKEN}`).then(res => res.json());
+    try {
+        content = res['files']['dns.json']['content']
+        window && ok(JSON.parse(content));
+        console.log('dddcg get success', content);
+    } catch (e) {
+        console.error('dddcg get nothing');
+        window && err(e);
+    }
+    return p;
 })();
